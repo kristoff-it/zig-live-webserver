0e0554e99e1e5e2dd039d383b071493a9186bf41
Scott Redig <git@scottredig.com>
2024-08-18 16:56:12 -0700

    iframe navbar working

--- src/script.js
+++ src/script.js
@@ -1,16 +1,37 @@
 let mainframe = document.getElementById("mainframe");
+let navbar = document.getElementById("navbar");
+
+let last_path_set = null;
+
+function pathChange(source, path) {
+    if (source === "hash" && path.length > 1) {
+        path = path.substring(1);
+    }
+    if (path === last_path_set) {
+        return;
+    }
+    last_path_set = path;
+    if (source !== "iframe") {
+        // Force iframe to local website, to avoid security errors
+        // related to accessing an iframe on a different domain.
+        mainframe.contentWindow.location = window.location.origin;
+        mainframe.contentWindow.location.pathname = path;
+    }
+    if (source !== "hash") {
+        window.location.hash = path;
+    }
+    if (source !== "navbar") {
+        navbar.value = path;
+    }
+}
 
 mainframe.addEventListener("load", function () {
-	hash_to_ignore = mainframe.contentWindow.location.pathname;
-	window.location.hash = mainframe.contentWindow.location.pathname;
+    pathChange("iframe", mainframe.contentWindow.location.pathname);
 });
-
-var hash_to_ignore = null;
-
 window.addEventListener("hashchange", function() {
-	if (hash_to_ignore == window.location.hash) {
-		mainframe.contentWindow.location = window.location;
-		mainframe.contentWindow.location.pathname = window.location.hash;
-	}
+    pathChange("hash", window.location.hash);
 });
-mainframe.contentWindow.location.pathname = window.location.hash;
+navbar.addEventListener("change", function() {
+    pathChange("navbar", navbar.value);
+});
+pathChange("hash", window.location.hash);
